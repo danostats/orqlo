c129cb9e710cd97793eef79f31e0ffe1cc74b74e
danostats <d.theobald@protonmail.com>
2026-07-01 14:17:32 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: { colors: { ink: '#080b12', panel: '#101624', line: '#243044', glow: '#8b5cf6' } } },
-  plugins: [],
-};
